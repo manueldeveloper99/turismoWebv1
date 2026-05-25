1d0513eb009f8fb34679c342f885ed383149c825
manueldeveloper99 <manueldeveloper99cs04@gmail.com>
2026-05-25 16:52:24 -0600

    Subiendo proyecto local de turismowebv1

---
+++ turismoweb/turismoweb/src/main/java/com/proyectouno/turismoweb/TurismowebApplication.java
@@ -0,0 +1,13 @@
+package com.proyectouno.turismoweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TurismowebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TurismowebApplication.class, args);
+	}
+
+}
