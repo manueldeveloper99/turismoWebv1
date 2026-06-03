7bca23df1a97f297905ae4b09a5427df19d2e4a0
manueldeveloper99 <manueldeveloper99cs04@gmail.com>
2026-06-03 16:12:08 -0600

    style: remove rounded bottom corners from navbar to make it flush with edges

--- frontend/src/components/NavbarComponent.jsx
+++ frontend/src/components/NavbarComponent.jsx
@@ -45,8 +45,6 @@ const NavbarComponent = () => {
     backdropFilter: 'blur(16px)',
     WebkitBackdropFilter: 'blur(16px)',
     boxShadow: '0 10px 30px rgba(0, 0, 0, 0.2)',
-    borderBottomLeftRadius: '25px',
-    borderBottomRightRadius: '25px',
     position: 'relative',
     overflow: 'hidden',
     zIndex: 1000,
