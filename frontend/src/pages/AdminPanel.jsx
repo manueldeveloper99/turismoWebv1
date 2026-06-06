354c3933eb4ae78f85d5404aeed255cdd745ed94
manueldeveloper99 <manueldeveloper99cs04@gmail.com>
2026-06-05 21:59:47 -0600

    Codigo actualizado

--- frontend/src/pages/AdminPanel.jsx
+++ frontend/src/pages/AdminPanel.jsx
@@ -370,9 +370,9 @@ const AdminPanel = () => {
                           variant="outline-primary"
                           size="sm"
                           className="me-2"
-                          onClick={() => { 
-                            setQrTown({ ...t, exactUrl: `${window.location.origin}/p/${t.slug}` }); 
-                            setShowQRModal(true); 
+                          onClick={() => {
+                            setQrTown({ ...t, exactUrl: `${window.location.origin}/p/${t.slug}` });
+                            setShowQRModal(true);
                           }}
                         >
                           <QrCode size={16} className="me-1" /> QR
