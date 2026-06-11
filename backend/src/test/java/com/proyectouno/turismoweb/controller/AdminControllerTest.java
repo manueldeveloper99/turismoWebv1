df6ea843f920fec1475f3916e35a3e913d959f02
manueldeveloper99 <manueldeveloper99cs04@gmail.com>
2026-06-11 00:12:03 -0600

    fix: replace @MockitoBean with @MockBean to support Spring Boot 3.3.6 in tests

--- backend/src/test/java/com/proyectouno/turismoweb/controller/AdminControllerTest.java
+++ backend/src/test/java/com/proyectouno/turismoweb/controller/AdminControllerTest.java
@@ -13,7 +13,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -38,11 +38,11 @@ public class AdminControllerTest {
     @Autowired
     private ObjectMapper objectMapper;
 
-    @MockitoBean
+    @MockBean
     private TownService townService;
-    @MockitoBean
+    @MockBean
     private PlaceService placeService;
-    @MockitoBean
+    @MockBean
     private UserService userService;
 
     /**
